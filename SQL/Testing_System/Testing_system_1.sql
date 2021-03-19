1f446a7d0dc469f25c0805b203ad490b27a828c7
Thuy19032000 <Luongdoanthuy1930@gmail.com>
2021-03-19 15:47:58 +0700

    Submit Assginment 1

--- SQL/Testing_System/Testing_system_1.sql
+++ SQL/Testing_System/Testing_system_1.sql
@@ -3,8 +3,8 @@ CREATE DATABASE IF NOT EXISTS Testing_System_Assignment_1;
 USE Testing_System_Assignment_1;
 
 CREATE TABLE Department (
-	Department_ID  			INT,
-    	Department_Name  		VARCHAR(50)
+    Department_ID  			INT,
+    Department_Name  		VARCHAR(50)
 );
 
 CREATE TABLE  Position (
@@ -14,25 +14,25 @@ CREATE TABLE  Position (
 
 CREATE TABLE  Account (
 	 Account_ID  			INT,
-     	 Email 				VARCHAR(50),
+     Email 				VARCHAR(50),
 	 User_Name  			VARCHAR(50),
-     	 Full_Name			VARCHAR(50),
-      	 Departmen_ID			INT,
-     	 Position_ID			INT,
-     	 Create_Date			DATE
+     Full_Name				VARCHAR(50),
+     Departmen_ID			INT,
+     Position_ID			INT,
+     Create_Date			DATE
 );
 
 CREATE TABLE   Groupj (
 	 Group_ID  			INT,
 	 Group_Name  			VARCHAR(50),
-         Create_ID			INT,
-         Create_Date			DATE
+     Create_ID				INT,
+     Create_Date			DATE
 );
 
 CREATE TABLE GroupAccount  (
 	 Group_ID  			INT,
 	 Account_ID  			INT,
-     	 Join_Date 			DATE
+     Join_Date 				DATE
 );
 
 CREATE TABLE  TypeQuestion  (
@@ -47,27 +47,27 @@ CREATE TABLE Category_Question  (
 CREATE TABLE Question (
 	 Question_ID  			INT,
 	 Content  			VARCHAR(50),
-     	 Category_ID 			INT,
-     	 Type_ID 			INT,
-     	 Creator_ID 			INT,
-     	 Create_Date 			DATE
+   	 Category_ID 			INT,
+     Type_ID 				INT,
+     Creator_ID 			INT,
+     Create_Date 			DATE
 );
 
 CREATE TABLE  Answer (
 	 Answer_ID 			INT,
 	 Content  			VARCHAR(50),
-    	 Question_ID 			INT,
-         is_Correct 			VARCHAR(50)
+     Question_ID 			INT,
+     is_Correct 			VARCHAR(50)
 );
 
 CREATE TABLE Exam  (
 	 Exam_ID  			INT,
 	 Code  				VARCHAR(50),
-     	 Title  			VARCHAR(50),
-    	 Category_ID  			INT,
-    	 Duration  			VARCHAR(10),
-    	 Creator_ID 			INT,
-    	 Create_Date 			DATE
+     Title  				VARCHAR(50),
+     Category_ID  			INT,
+     Duration  				VARCHAR(10),
+     Creator_ID 			INT,
+     Create_Date 			DATE
 );
 
 CREATE TABLE ExamQuestion  (
